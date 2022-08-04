44cc2f635c540119c2f57837117625cd612ef5fb
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2022-08-05 01:14:37 +0300

    implement other tags, with tests

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,10 +5,22 @@ require_relative "hexlet_code/version"
 module HexletCode
   # Generate HTML tag
   class Tag
-    def self.build(name)
-      return "<#{name}>" if name.eql? "br"
+    SINGLE_TAGS = %w[br img input].freeze
 
-      "<#{name}></#{name}>"
+    def self.generate_attributes_line(attributes = {})
+      attr_pairs = attributes.any? ? [""] : []
+      attributes.each do |key, value|
+        attr_pairs << "#{key}=\"#{value}\""
+      end
+      attr_pairs.join(" ")
+    end
+
+    def self.build(name, attributes = {})
+      attr_line = generate_attributes_line(attributes)
+      return "<#{name}#{attr_line}>" if SINGLE_TAGS.include? name
+
+      body = yield if block_given?
+      "<#{name}#{attr_line}>#{body}</#{name}>"
     end
   end
 end
--- test/lib/test_hexlet_code.rb
+++ test/lib/test_hexlet_code.rb
@@ -7,11 +7,28 @@ class TestHexletCode < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_that_it_can_generate_empty_pairt_tag
-    assert { HexletCode::Tag.build("div").eql? "<div></div>" }
+  def test_that_it_can_generate_br
+    assert { HexletCode::Tag.build("br").eql? "<br>" }
   end
 
-  def test_that_it_can_generate_br_tag
-    assert { HexletCode::Tag.build("br").eql? "<br>" }
+  def test_that_it_can_generate_img
+    assert { HexletCode::Tag.build("img", src: "path/to/image").eql? '<img src="path/to/image">' }
+  end
+
+  def test_that_it_can_generate_input
+    assert { HexletCode::Tag.build("input", type: "submit", value: "Save").eql? '<input type="submit" value="Save">' }
+  end
+
+  def test_that_it_can_generate_label
+    assert { HexletCode::Tag.build("label") { "Email" }.eql? "<label>Email</label>" }
+  end
+
+  def test_that_it_can_generate_label_with_attribute
+    assert { HexletCode::Tag.build("label", for: "email") { "Email" }.eql? '<label for="email">Email</label>' }
+  end
+
+
+  def test_that_it_can_generate_div
+    assert { HexletCode::Tag.build("div").eql? "<div></div>" }
   end
 end
