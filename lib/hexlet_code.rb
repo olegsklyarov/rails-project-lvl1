c9d13b458f340380c12cf0cd94f1582a4edddfe9
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2022-09-12 18:34:31 +0300

    move tag class to separate file

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,30 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require_relative "hexlet_code/tag"
 
-# HexletCode module
 module HexletCode
-  # Generate HTML tag
-  class Tag
-    SINGLE_TAGS = %w[br img input].freeze
-
-    def self.generate_attributes_line(attributes = {})
-      attr_pairs = attributes.any? ? [""] : []
-      attributes.each do |key, value|
-        attr_pairs << "#{key}=\"#{value}\""
-      end
-      attr_pairs.join(" ")
-    end
-
-    def self.build(name, attributes = {})
-      attr_line = generate_attributes_line(attributes)
-      return "<#{name}#{attr_line}>" if SINGLE_TAGS.include? name
-
-      body = yield if block_given?
-      "<#{name}#{attr_line}>#{body}</#{name}>"
-    end
-  end
-
   def self.form_for(entity, attributes = {})
     @entity = entity
     @inputs = []
--- test/lib/test_hexlet_code.rb
+++ test/lib/test_hexlet_code.rb
@@ -13,30 +13,6 @@ class TestHexletCode < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_that_it_can_generate_br
-    assert { HexletCode::Tag.build("br").eql? "<br>" }
-  end
-
-  def test_that_it_can_generate_img
-    assert { HexletCode::Tag.build("img", src: "path/to/image").eql? '<img src="path/to/image">' }
-  end
-
-  def test_that_it_can_generate_input
-    assert { HexletCode::Tag.build("input", type: "submit", value: "Save").eql? '<input type="submit" value="Save">' }
-  end
-
-  def test_that_it_can_generate_label
-    assert { HexletCode::Tag.build("label") { "Email" }.eql? "<label>Email</label>" }
-  end
-
-  def test_that_it_can_generate_label_with_attribute
-    assert { HexletCode::Tag.build("label", for: "email") { "Email" }.eql? '<label for="email">Email</label>' }
-  end
-
-  def test_that_it_can_generate_div
-    assert { HexletCode::Tag.build("div").eql? "<div></div>" }
-  end
-
   def test_that_it_can_generate_form
     form_html = HexletCode.form_for @user do |f|
     end
